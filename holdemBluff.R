937716fbbd07654b4dde7e0590e1e154c77b23d0
boringmachine <machine449@gmail.com>
2013-04-22 11:41:06 +0900

    update

--- holdemBluff.R
+++ holdemBluff.R
@@ -1,7 +1,12 @@
+# 混合戦略のMinMax解
 prob <- function(a1,a2,a3,a4){
     return((a4-a3)/(a1-a2+a4-a3));
 }
 
+# x: 勝率
+# a: ポット
+# d: 不確実性に対する感度
+# t: バンクロールに対するブラフ時の投資額の率
 probgame <- function(x,d,t,a){
     a1 <- x**d+t+a;
     a2 <- a;
