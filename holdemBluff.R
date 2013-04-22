8a4835b252f1bfa497ef0d6b208f9ebc003fc77c
boringmachine <machine449@gmail.com>
2013-04-22 14:23:25 +0900

    update

--- holdemBluff.R
+++ holdemBluff.R
@@ -7,11 +7,11 @@ prob <- function(a1,a2,a3,a4){
 # a: アンティ
 # d: 不確実性に対する感度
 # t: バンクロールに対するブラフ時の追加投資額の率
-probgame <- function(x,d,t,a){
-    a1 <- x**d+t+a;
+probgame <- function(x,d,t,a,i){
+    a1 <- i*(x**d)+t+a;
     a2 <- a;
     a3 <- -a;
-    a4 <- x**d+a;
+    a4 <- i*(x**d)+a;
     a1 <- a1*(2*x-1);
     a4 <- a4*(2*x-1);
     return(prob(a1,a2,a3,a4));
