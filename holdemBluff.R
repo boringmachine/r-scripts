c90d1bd25f6665d0797e9901ae203f87897dc81d
boringmachine <machine449@gmail.com>
2013-04-22 11:44:45 +0900

    update

--- holdemBluff.R
+++ holdemBluff.R
@@ -12,7 +12,7 @@ probgame <- function(x,d,t,a){
     a2 <- a;
     a3 <- -a;
     a4 <- x**d+a;
-    a1 <- a1/(2*x-1);
-    a4 <- a4/(2*x-1);
+    a1 <- a1*(2*x-1);
+    a4 <- a4*(2*x-1);
     return(prob(a1,a2,a3,a4));
 }
